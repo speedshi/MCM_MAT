86510007667b333f467ae1a9f1ec15d404ed96aa
Peidong Shi <speedshi@hotmail.com>
2020-04-10 20:04:33 +0200

    Add files via upload

--- libs/callyifdp.m
+++ libs/callyifdp.m
@@ -1,13 +1,13 @@
 function lydp=callyifdp(thickness,rsd0)
 % This function is used to calculate the layer depth of each interface for
-% the layered model.
+% the layered model (including the free surface).
 % Unit: meter.
 % INPUT--------------------------------------------------------------------
 % rsd0: the depth of the free surface (top of the first layer),
 % 0-->sea-level, (+)positive-->below sea-level, (-)negative-->above sea-level;
 % thickness: the thickness of each layer, a vector.
 % OUTPUT-------------------------------------------------------------------
-% lydp: the depths of each layer interface (lay boundaries).
+% lydp: the depths of each layer interface (lay boundaries) including the free surface.
 
 % set default value
 if nargin < 2
